bf181dc8456cd388714acbd88f4ff7a6149b0fbf
Csingh26 <singh.chaittanya@gmail.com>
2024-06-19 10:16:13 +0530

    signup-form

--- frontend/src/components/Signup/signup.jsx
+++ frontend/src/components/Signup/signup.jsx
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../Header/header";
 import './signup.css'
 import { Link } from "react-router-dom";
 
 function SignupForm() {
+    const [formData,setFormData] = useState({
+        email: '',
+        username: '',
+        password: '',
+        confirmPassword: ''
+    })
+
+    const [errors, setErrors] = useState({
+        password: '',
+        confirmPassword: ''
+    })
+
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        setFormData({ ...formData, [name]: value})
+    }
+
+    const validatePwd = (password) => {
+        const lenChk = password.length >= 8
+        const uprCaseChk = /[A-Z]/.test(password)
+        const spcChar = /[!@#$%^&*(),.?{}|]/.test(password)
+        const numChk = /[0-9]/.test(password)
+        return lenChk && uprCaseChk && spcChar && numChk
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(formData)
+    }
     return (
         <>
             <Header />
@@ -16,6 +45,9 @@ function SignupForm() {
                             type="email" 
                             placeholder="Email"
                             className="email"
+                            name="email"
+                            value={formData.email}
+                            onChange={handleChange}
                             />
                         </div>
                         <div className="username-input">
@@ -23,6 +55,9 @@ function SignupForm() {
                             type="text" 
                             placeholder="Username"
                             className="username"
+                            name="username"
+                            value={formData.username}
+                            onChange={handleChange}
                             />
                         </div>
                         <div className="password-input">
@@ -30,6 +65,9 @@ function SignupForm() {
                             type="password" 
                             placeholder="Password"
                             className="password"
+                            name="password"
+                            value={formData.password}
+                            onChange={handleChange}
                             />
                         </div>
                         <div className="password-input">
@@ -38,6 +76,9 @@ function SignupForm() {
                             placeholder="Confirm Password"
                             className="password"
                             id="confirm-pwd"
+                            name="confirmPassword"
+                            value={formData.confirmPassword}
+                            onChange={handleChange}
                             />
                         </div>
                         <div className="forgot-pwd">
